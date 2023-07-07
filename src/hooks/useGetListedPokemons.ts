5e5ca49738ef3fdedb8d823bb936c6736831bf4e
Digudev <1.5.dg.diego@gmail.com>
2023-07-07 10:31:43 -0600

    FEAT: Fetch pokemon list hook added

---
+++ src/hooks/useGetListedPokemons.ts
@@ -0,0 +1,41 @@
+import {useEffect, useRef, useState} from 'react';
+import {pokeApi} from '../api';
+import {ListedPokemon, PokemonPaginated, BasicPokemon} from '../interfaces';
+
+export const useGetListedPokemons = () => {
+  const [basicPokemonsList, setBasicPokemonsList] = useState<BasicPokemon[]>(
+    [],
+  );
+  const pokemonPageUrl = useRef(
+    'https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0',
+  );
+
+  const fetchListedPokemons = async () => {
+    const response = await pokeApi.get<PokemonPaginated>(
+      pokemonPageUrl.current,
+    );
+    pokemonPageUrl.current = response.data.next;
+
+    createBasicPokemons(response.data.results);
+  };
+
+  const createBasicPokemons = (listedPokemons: ListedPokemon[]) => {
+    const basicPokemons = listedPokemons.map(pokemon => {
+      const splitedUrl = pokemon.url.split('/');
+      const id = splitedUrl[splitedUrl.length - 2];
+      const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+
+      return {id, name: pokemon.name, picture};
+    });
+
+    setBasicPokemonsList([...basicPokemonsList, ...basicPokemons]);
+  };
+
+  useEffect(() => {
+    fetchListedPokemons();
+  }, []);
+
+  console.log(basicPokemonsList);
+
+  return {basicPokemonsList};
+};
